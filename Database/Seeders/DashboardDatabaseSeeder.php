c855ac46a1c620f379ad8850dc9eb24c2d8fb198
Nicolas Widart <n.widart@gmail.com>
2015-02-11 22:06:29 +0100

    PSR-2

--- Database/Seeders/DashboardDatabaseSeeder.php
+++ Database/Seeders/DashboardDatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php namespace Modules\Dashboard\Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
 class DashboardDatabaseSeeder extends Seeder
 {
