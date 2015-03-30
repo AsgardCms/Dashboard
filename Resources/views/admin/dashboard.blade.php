bf7c1e2d6abd012afea40cab111e5965ade1cd15
Nicolas Widart <n.widart@gmail.com>
2015-03-30 08:27:59 +0200

    Adding jquery ui and lodash

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -24,4 +24,30 @@
             <?php endforeach; ?>
         <?php endif; ?>
     </div>
+    <div class="row">
+        <div class="grid-stack">
+            <div class="grid-stack-item"
+                 data-gs-x="0" data-gs-y="0"
+                 data-gs-width="4" data-gs-height="2">
+                <div class="grid-stack-item-content">asdasd</div>
+            </div>
+            <div class="grid-stack-item"
+                 data-gs-x="4" data-gs-y="0"
+                 data-gs-width="4" data-gs-height="4">
+                <div class="grid-stack-item-content">asdasd</div>
+            </div>
+        </div>
+    </div>
+@stop
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function () {
+            var options = {
+                cell_height: 80,
+                vertical_margin: 10
+            };
+            $('.grid-stack').gridstack(options);
+        });
+    </script>
 @stop
