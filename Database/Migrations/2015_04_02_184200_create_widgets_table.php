175630096f9ed85e5c61ba05d9da86600d707f94
Nicolas Widart <n.widart@gmail.com>
2015-04-02 20:45:11 +0200

    Create migration for a widgets table

---
+++ Database/Migrations/2015_04_02_184200_create_widgets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateWidgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dashboard__widgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->text('widgets');
+
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dashboard__widgets');
+    }
+}
