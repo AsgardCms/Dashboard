9ca19773e9d90dacfd13ac650302981c8d62da3d
franciscpd <franciscpd@gmail.com>
2015-07-19 16:54:56 -0400

    Adding portuguese brasilian locale

---
+++ Resources/lang/pt-BR/dashboard.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => 'Painel',
+    'edit grid' => 'Editar grid',
+    'reset grid' => 'Redefinir grid',
+    'add widget' => 'Adicionar widget',
+    'add widget to dashboard' => 'Adicionar widget no painel',
+    'reset not needed' => 'Painel não redefinido',
+    'dashboard reset' => 'Painel foi redefinido',
+];
