6be0e2b795cc7e729b8ea4038adfa8caa98a7653
Nicolas Widart <n.widart@gmail.com>
2015-12-29 21:13:54 +0100

    Reverting change

--- Database/Migrations/2015_04_02_184200_create_widgets_table.php
+++ Database/Migrations/2015_04_02_184200_create_widgets_table.php
@@ -18,6 +18,7 @@ class CreateWidgetsTable extends Migration
             $table->text('widgets');
 
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on(config('auth.table', 'users'))->onDelete('cascade');
         });
     }
 
